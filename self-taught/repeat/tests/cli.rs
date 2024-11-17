409f4eec4cbb03c4ccd8f0e6180c8407ea6afba2
ftnext <takuyafjp+develop@gmail.com>
2024-11-17 20:24:01 +0900

    [test] Specify string only

--- self-taught/repeat/tests/cli.rs
+++ self-taught/repeat/tests/cli.rs
@@ -6,3 +6,10 @@ fn 文字列と回数を指定して繰り返す() {
     let assert = cmd.arg("大好き").arg("5").assert();
     assert.success().stdout("大好き大好き大好き大好き大好き\n");
 }
+
+#[test]
+fn 文字列だけ指定した場合は3回繰り返す() {
+    let mut cmd = Command::cargo_bin("repeat").unwrap();
+    let assert = cmd.arg("yaruzo-").assert();
+    assert.success().stdout("yaruzo-yaruzo-yaruzo-\n");
+}
