71aaae52d43c1cb53cf7fda8a8d8c8dc4a99c5a0
ftnext <takuyafjp+develop@gmail.com>
2025-01-13 17:38:36 +0900

    [feat] Not worked: clap with entry point script
    >error: unrecognized subcommand

--- self-taught/repeatpy/src/lib.rs
+++ self-taught/repeatpy/src/lib.rs
@@ -1,14 +1,43 @@
 use pyo3::prelude::*;
+// use pyo3::{prelude::{pyfunction, pymodule}, types::{PyModule, PyModuleMethods}, wrap_pyfunction, Bound, PyResult};
+use clap::{Parser, Subcommand};
 
-/// Formats the sum of two numbers as string.
-#[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
+#[derive(Parser)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
 }
 
-/// A Python module implemented in Rust.
+#[derive(Subcommand)]
+enum Commands {
+    Position {
+        string: String,
+        #[arg(default_value_t = 3)]
+        number: usize,
+    },
+    Option {
+        #[arg(short, long)]
+        string: String,
+        #[arg(short, long, default_value_t = 3)]
+        number: usize,
+    },
+}
+
+#[pyfunction]
+fn repeat_cli() {
+    let args = Cli::parse();
+    match args.command {
+        Commands::Position { string, number } => {
+            println!("{}", string.repeat(number));
+        }
+        Commands::Option { string, number } => {
+            println!("{}", string.repeat(number));
+        }
+    }
+}
+
 #[pymodule]
 fn repeatpy(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(repeat_cli, m)?)?;
     Ok(())
 }
