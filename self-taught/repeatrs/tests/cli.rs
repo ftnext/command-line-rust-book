fbe17571823e442e1679e4a39c2e95f4566e3bc7
ftnext <takuyafjp+develop@gmail.com>
2025-01-14 18:43:24 +0900

    [refactor] Rename repeatrs (repeat is reserved word)

--- self-taught/repeatrs/tests/cli.rs
+++ self-taught/repeatrs/tests/cli.rs
@@ -2,21 +2,21 @@ use assert_cmd::Command;
 
 #[test]
 fn positionコマンド_文字列と回数を指定して繰り返す() {
-    let mut cmd = Command::cargo_bin("repeat").unwrap();
+    let mut cmd = Command::cargo_bin("repeatrs").unwrap();
     let assert = cmd.arg("position").arg("大好き").arg("5").assert();
     assert.success().stdout("大好き大好き大好き大好き大好き\n");
 }
 
 #[test]
 fn positionコマンド_文字列だけ指定した場合は3回繰り返す() {
-    let mut cmd = Command::cargo_bin("repeat").unwrap();
+    let mut cmd = Command::cargo_bin("repeatrs").unwrap();
     let assert = cmd.arg("position").arg("yaruzo-").assert();
     assert.success().stdout("yaruzo-yaruzo-yaruzo-\n");
 }
 
 #[test]
 fn optionコマンド_文字列と回数を指定して繰り返す() {
-    let mut cmd = Command::cargo_bin("repeat").unwrap();
+    let mut cmd = Command::cargo_bin("repeatrs").unwrap();
     let assert = cmd
         .arg("option")
         .arg("-s")
@@ -29,7 +29,7 @@ fn optionコマンド_文字列と回数を指定して繰り返す() {
 
 #[test]
 fn optionコマンド_文字列だけ指定した場合は3回繰り返す() {
-    let mut cmd = Command::cargo_bin("repeat").unwrap();
+    let mut cmd = Command::cargo_bin("repeatrs").unwrap();
     let assert = cmd.arg("option").arg("-s").arg("yaru").assert();
     assert.success().stdout("yaruyaruyaru\n");
 }
