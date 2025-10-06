875793628af4a1ef83699d0ae6df5735203c1ddb
ftnext <takuyafjp+develop@gmail.com>
2025-10-06 21:18:24 +0900

    [feat] Hello, Logfire!

--- self-taught/hello-logfire/src/main.rs
+++ self-taught/hello-logfire/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use logfire::config::ConsoleOptions;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let logfire = logfire::configure()
+        .with_console(Some(ConsoleOptions::default()))
+        .finish()?;
+
+    let _guard = logfire.shutdown_guard();
+
+    logfire::info!("Hello, world!");
+
+    Ok(())
 }
