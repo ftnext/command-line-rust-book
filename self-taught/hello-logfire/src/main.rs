b689e84107a6925919fbfe628de0385babbe3fc8
ftnext <takuyafjp+develop@gmail.com>
2025-10-13 11:07:42 +0900

    [feat] Use with logfire.tracing_layer()

--- self-taught/hello-logfire/src/main.rs
+++ self-taught/hello-logfire/src/main.rs
@@ -1,5 +1,7 @@
-// use logfire::config::ConsoleOptions;
+// https://github.com/pydantic/logfire-rust/blob/v0.8.2/src/logfire.rs#L129-L144
+use logfire::config::ConsoleOptions;
 use tracing::{Level, event, instrument};
+use tracing_subscriber::layer::SubscriberExt;
 
 #[instrument]
 fn my_function(my_arg: usize) {
@@ -11,10 +13,19 @@ fn my_function(my_arg: usize) {
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let logfire = logfire::configure()
-        // .with_console(Some(ConsoleOptions::default()))
-        .finish()?;
-    let _guard = logfire.shutdown_guard();
+        .local()
+        .with_console(Some(ConsoleOptions::default()))
+        .finish()
+        .expect("Failed to configure logfire");
 
+    // local() の上で macro を記録するための設定
+    let _guard = logfire::set_local_logfire(logfire.clone());
+    logfire::info!("Hello world!");
+
+    let subscriber = tracing_subscriber::registry().with(logfire.tracing_layer());
+    tracing::subscriber::set_global_default(subscriber).expect("Failed to set global subscriber");
     my_function(1);
+
+    logfire.shutdown().expect("Failed to shutdown logfire");
     Ok(())
 }
