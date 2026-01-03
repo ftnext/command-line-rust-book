62b83730f4ec464c9dc42622625fd934e8f65dac
ftnext <takuyafjp+develop@gmail.com>
2026-01-03 12:14:01 +0900

    feat: rustpython_ast visitor feature example
    https://github.com/RustPython/Parser/issues/108#issuecomment-2470479057

--- self-taught/hello-python-ast/src/main.rs
+++ self-taught/hello-python-ast/src/main.rs
@@ -1,8 +1,32 @@
-use rustpython_parser::{ast, Parse};
+use rustpython_ast::{ExprAttribute, Visitor};
+use rustpython_parser::{Parse, ast};
+
+struct AttributeCounter {
+    attributes_count: usize,
+}
+
+impl Visitor for AttributeCounter {
+    fn visit_expr_attribute(&mut self, node: ExprAttribute) {
+        self.attributes_count += 1;
+        self.generic_visit_expr_attribute(node);
+    }
+}
 
 fn main() {
-    let python_source = "if True: pass # comment";
-    let ast = ast::Suite::parse(python_source, "<embedded>");
-    assert!(ast.is_ok());
-    println!("{:#?}", ast.unwrap());
+    let python_source = r#"
+def greet(person):
+    return person.name + " " + person.age
+"#;
+    let ast = ast::Suite::parse(python_source, "<embedded>").unwrap();
+    println!("{:#?}", ast);
+
+    let mut counter = AttributeCounter {
+        attributes_count: 0,
+    };
+    println!("Attributes count: {}", counter.attributes_count);
+    for stmt in ast {
+        counter.visit_stmt(stmt);
+    }
+
+    println!("Attributes count: {}", counter.attributes_count);
 }
