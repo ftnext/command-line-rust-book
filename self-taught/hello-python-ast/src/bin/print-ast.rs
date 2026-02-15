78b1e7cd45034ad6f16ec0bb0e1ecdfce235106c
ftnext <takuyafjp+develop@gmail.com>
2026-02-15 17:01:27 +0900

    feat: lint list type hint by GPT 5.3 Codex

--- self-taught/hello-python-ast/src/bin/print-ast.rs
+++ self-taught/hello-python-ast/src/bin/print-ast.rs
@@ -1,9 +1,9 @@
-use rustpython_parser::{ast, Parse};
+use rustpython_parser::{Parse, ast};
 
 fn main() {
     let python_source = r#"
-def greet(person):
-    return person.name + " " + person.age
+def plus_one_ng(numbers: list[int]) -> list[int]:
+    return [n + 1 for n in numbers]
 "#;
     let ast = ast::Suite::parse(python_source, "<embedded>");
     assert!(ast.is_ok());
--- self-taught/hello-python-ast/src/main.rs
+++ self-taught/hello-python-ast/src/main.rs
@@ -1,4 +1,4 @@
-use rustpython_ast::{ExprAttribute, Visitor};
+use rustpython_ast::{Arguments, ExprAttribute, Ranged, Visitor};
 use rustpython_parser::{Parse, ast};
 
 struct AttributeCounter {
@@ -12,20 +12,61 @@ impl Visitor for AttributeCounter {
     }
 }
 
+struct KotohaVisitor<'a> {
+    source: &'a str,
+}
+
+fn byte_offset_to_line_col(source: &str, offset: usize) -> (usize, usize) {
+    let prefix = &source[..offset];
+    let line = prefix.bytes().filter(|&b| b == b'\n').count() + 1;
+    let col = match prefix.rfind('\n') {
+        Some(last_newline) => offset - last_newline,
+        None => offset + 1,
+    };
+    (line, col)
+}
+
+impl Visitor for KotohaVisitor<'_> {
+    fn visit_arguments(&mut self, node: Arguments) {
+        for arg in &node.args {
+            if let Some(annotation) = &arg.def.annotation {
+                if let rustpython_ast::Expr::Subscript(subscript) = annotation.as_ref() {
+                    if let rustpython_ast::Expr::Name(name) = subscript.value.as_ref() {
+                        if name.id.as_str() == "list" {
+                            let range = annotation.range();
+                            let annotation_text =
+                                &self.source[usize::from(range.start())..usize::from(range.end())];
+                            let (line, col) = byte_offset_to_line_col(
+                                self.source,
+                                usize::from(arg.def.range().start()),
+                            );
+                            println!(
+                                "Fix type hint `{}: {}` at {}:{}",
+                                arg.def.arg, annotation_text, line, col
+                            );
+                        }
+                    }
+                }
+            }
+        }
+        self.generic_visit_arguments(node);
+    }
+}
+
 fn main() {
     let python_source = r#"
-def greet(person):
-    return person.name + " " + person.age
+def plus_one_ng(numbers: list[int]) -> list[int]:
+    return [n + 1 for n in numbers]
+
+def plus_one_ok(numbers: Iterable[int]) -> list[int]:
+    return [n + 1 for n in numbers]
 "#;
     let ast = ast::Suite::parse(python_source, "<embedded>").unwrap();
 
-    let mut counter = AttributeCounter {
-        attributes_count: 0,
+    let mut visitor = KotohaVisitor {
+        source: python_source,
     };
-    println!("Attributes count: {}", counter.attributes_count);
     for stmt in ast {
-        counter.visit_stmt(stmt);
+        visitor.visit_stmt(stmt);
     }
-
-    println!("Attributes count: {}", counter.attributes_count);
 }
