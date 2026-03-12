6ceaa5cbcd523fe368e12404c47ff8668ebebac3
ftnext <takuyafjp+develop@gmail.com>
2026-03-12 12:48:42 +0900

    feat: two_face example

--- self-taught/codex-highlight/src/main.rs
+++ self-taught/codex-highlight/src/main.rs
@@ -1,3 +1,17 @@
+use two_face::re_exports::syntect;
+
+const TOML_TEXT: &str = "\
+[section]
+key = 123
+";
+
 fn main() {
-    println!("Hello, world!");
+    let syn_set = two_face::syntax::extra_newlines();
+    let theme_set = two_face::theme::extra();
+
+    let syn_ref = syn_set.find_syntax_by_extension("toml").unwrap();
+    let theme = &theme_set[two_face::theme::EmbeddedThemeName::Nord];
+    let htmlified =
+        syntect::html::highlighted_html_for_string(TOML_TEXT, &syn_set, syn_ref, theme).unwrap();
+    println!("{}", htmlified);
 }
