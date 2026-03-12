9f83263f671f2bf04a384508c59b02d484ebc589
ftnext <takuyafjp+develop@gmail.com>
2026-03-12 12:56:34 +0900

    feat: Highlight python

--- self-taught/codex-highlight/src/main.rs
+++ self-taught/codex-highlight/src/main.rs
@@ -1,18 +1,21 @@
 use two_face::re_exports::syntect;
 
-const TOML_TEXT: &str = "\
-[section]
-key = 123
-";
+const PYTHON_SOURCE: &str = r#"
+def plus_one_ng(numbers: list[int]) -> list[int]:
+    return [n + 1 for n in numbers]
+
+def plus_one_ok(numbers: Iterable[int]) -> list[int]:
+    return [n + 1 for n in numbers]
+"#;
 
 fn main() {
     let syn_set = two_face::syntax::extra_newlines();
     let theme_set = two_face::theme::extra();
 
-    let syn_ref = syn_set.find_syntax_by_extension("toml").unwrap();
+    let syn_ref = syn_set.find_syntax_by_extension("py").unwrap();
     let theme = &theme_set[two_face::theme::EmbeddedThemeName::Nord];
     let mut highlighter = syntect::easy::HighlightLines::new(syn_ref, theme);
-    let lines = TOML_TEXT.lines();
+    let lines = PYTHON_SOURCE.lines();
     let mut output = String::new();
 
     for line in lines {
